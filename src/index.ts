df0ff1fe46eb88e68a78fd1c97b7cdf79fc8dba8
zilurrane <zilurrane@gmail.com>
2022-05-10 21:46:17 +0530

    Connect to MongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
+import mongoose from 'mongoose';
 import { app } from './app';
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 7000;
+const MONGO_URI = process.env.MONGO_URI;
 
 const start = async () => {
+    if (!MONGO_URI) {
+        throw new Error('MONGO_URI must be defined');
+    }
+    try {
+        await mongoose.connect(MONGO_URI);
+        console.log('Connected to MongoDB.....!!!!!');
+    } catch (err) {
+        console.error(err);
+    }
     app.listen(PORT, () => {
-        console.log(`Service listening on PORT: ${PORT}`);
+        console.log(`Auth service listening on PORT: ${PORT}`);
     });
 };
 
