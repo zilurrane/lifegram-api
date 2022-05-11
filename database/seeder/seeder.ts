2516b8d6caaff1b8ea8c1a8cf13a407cc9b7db4a
zilurrane <zilurrane@gmail.com>
2022-05-11 22:08:27 +0530

    Added dummy user followers data

--- database/seeder/seeder.ts
+++ database/seeder/seeder.ts
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 const readlineSync = require("readline-sync");
 require("dotenv/config");
 const User = require("../../src/models/user.model.ts");
+const UserFollower = require("../../src/models/user-follower.model.ts");
 const userData = require("./users.json");
+const userFollowersData = require("./user-followers.json");
 
 console.log("START : MongoDB Seeder....");
 console.log("This will insert seed data into your database.");
@@ -23,12 +25,19 @@ const doDataSeeding = async () => {
     await mongoose.connect(MONGO_URI);
     console.log('Connected to MongoDB.....!!!!!');
 
-    console.log(userData);
-
-    for(let i = 0; i < userData?.length; i++) {
-        const userItem = userData[i];
-        await (new User.User(userItem)).save();
+    for (let i = 0; i < userData?.length; i++) {
+        const record = userData[i];
+        await (new User.User(record)).save();
     }
+
+    console.log(`Inserted ${userData.length} users.....`);
+
+    for (let i = 0; i < userFollowersData?.length; i++) {
+        const record = userFollowersData[i];
+        await (new UserFollower.UserFollower(record)).save();
+    }
+
+    console.log(`Inserted ${userData.length} user-follower entries.....`);
 
 }
 
