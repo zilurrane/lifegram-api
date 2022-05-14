6a32e38f2ed76c105c23d5ed5bd4fe1523d9fa86
zilurrane <zilurrane@gmail.com>
2022-05-14 20:07:42 +0530

    123456 OTP in development region

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { generateOTP } from "../services/miscellaneous.service";
 import { upsertUserOTP } from "../services/user-otp.service";
 import { createUser, getUserByPhoneNumber } from "../services/user.service";
+import { isDevelopment } from "../shared/utils/node-utils";
 
 export const handlePhoneNumber = async (req: Request, res: Response, next: NextFunction) => {
     const { phoneNumber } = req.body;
@@ -11,7 +12,7 @@ export const handlePhoneNumber = async (req: Request, res: Response, next: NextF
             user = await createUser({ phoneNumber, isActive: false, isVerified: false, createdBy: null, modifiedBy: null });
         }
 
-        const otp = generateOTP(6, { lowerCaseAlphabets: false, upperCaseAlphabets: false, specialChars: false });
+        const otp = isDevelopment() ? "123456" : generateOTP(6, { lowerCaseAlphabets: false, upperCaseAlphabets: false, specialChars: false });
         const expiry = new Date((new Date()).getTime() + 15 * 60000);
 
         await upsertUserOTP(user.id, { userId: user.id, otp, expiry, status: "PENDING", createdBy: null, modifiedBy: null });
