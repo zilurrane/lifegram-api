27d1daa426b8071f6ef22728f256ecc4331ca3da
zilurrane <zilurrane@gmail.com>
2022-06-04 09:58:03 +0530

    Upload image to FileSystem using multer

--- src/routes/post.route.ts
+++ src/routes/post.route.ts
@@ -1,9 +1,20 @@
-import express from 'express';
+import express, { Request } from 'express';
 import { handleCreatePost, handleGetAllPosts } from '../controllers/post.controller';
+
+const multer = require("multer");
+const storage = multer.diskStorage({
+    destination: function (req: Request, file: any, cb: any) {
+        cb(null, './uploads')
+    },
+    filename: function (req: Request, file: any, cb: any) {
+        cb(null, file.originalname)
+    }
+});
+const upload = multer({ storage: storage })
 
 const router = express.Router();
 
 router.get('/', handleGetAllPosts);
-router.post('/', handleCreatePost);
+router.post('/', upload.single('file'), handleCreatePost);
 
 export default router;
